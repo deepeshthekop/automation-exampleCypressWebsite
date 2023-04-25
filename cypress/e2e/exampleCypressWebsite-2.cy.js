24bfd5978b8f369106512e275185ad838bc60ce7
Deepesh Subedi <deepeshthekop8@gmail.com>
2023-04-25 12:57:15 +0545

    made changes to the exampleCypressWebsite-2 file

--- cypress/e2e/exampleCypressWebsite-2.cy.js
+++ cypress/e2e/exampleCypressWebsite-2.cy.js
@@ -1,5 +1,15 @@
 it ("example cypress website automation-2", () => {
 
-    
+    cy.visit('https://example.cypress.io/')
+    cy.get('.navbar-collapse.collapse').within(() => {
+        cy.contains('GitHub')
+        .click()
+    })
+    cy.origin('https://github.com', () => {
+        cy.get('#readme').within(() => {
+            cy.contains('Follow these instructions to install and write tests in Cypress.')
+            .click()
+        })
+    })
 
 })
